e894ca66b6728cf291d309f432a7b0e7a08a7208
曹绪培 <669307582@qq.com>
2024-06-12 14:47:06 +0800

    feat: 资源优化配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
-// https://vitejs.dev/config/
+import viteCompression from 'vite-plugin-compression'
+import seoPrerender from 'vite-plugin-seo-prerender'
+
+const routes = [
+  { path: '/' },
+  // 其他路由...
+]
+// 提取路由路径
+function getPrerenderRoutes() {
+  return routes.map(route => route.path)
+}
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    viteCompression({
+      verbose: true, // 启用日志输出，可以设置为 false 关闭
+      disable: false, // 是否禁用压缩，默认为 false，即启用压缩
+      deleteOriginFile: false, // 是否删除源文件，默认为 false，即不删除
+      threshold: 5120, // 压缩前最小文件大小，单位为字节，小于该值的文件不会被压缩
+      algorithm: 'gzip', // 压缩算法，这里使用 gzip
+      ext: '.gz' // 压缩后的文件扩展名
+    }),
+    seoPrerender({
+      routes: getPrerenderRoutes() // 需要生成的路由
+    })],
   server: {
     proxy: {
       '/api': {
@@ -29,6 +50,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets", //指定静态资源存放路径
+    minify: 'terser', // 使用Terser压缩JavaScript
     terserOptions: {
       // 生产环境移除console
       compress: {
@@ -36,5 +58,16 @@ export default defineConfig({
         drop_debugger: true,
       },
     },
+    // 浏览器兼容性配置
+    target: 'esnext',
+    // 产物拆包配置
+    rollupOptions: {
+      output: {
+        // 控制 chunk 的生成和命名
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
   },
 })
