ed62831bd2c16e16a80e50419cd02612ac3bf5f3
曹绪培 <669307582@qq.com>
2024-06-13 11:07:04 +0800

    feat: loading动画替换

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import LoveHeartLoading from "@/components/csszujian/LoveHeartLoading.vue";
+import YtLoadingComponents from "@/components/csszujian/YtLoadingComponents.vue";
 </script>
 
 <template>
-  <LoveHeartLoading/>
+<!--  <LoveHeartLoading/>-->
 <!--  页面数据渲染之后再加载-->
+  <YtLoadingComponents/>
   <RouterView />
 </template>
 
