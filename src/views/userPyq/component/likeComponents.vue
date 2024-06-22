1016c8a3fb0728303434d7330c93a6ee21b3afca
曹绪培 <669307582@qq.com>
2024-06-22 22:44:00 +0800

    fix: 朋友圈点赞功能bug修改

--- src/views/userPyq/component/likeComponents.vue
+++ src/views/userPyq/component/likeComponents.vue
@@ -6,10 +6,10 @@
     <div class="post-fun g-right">
       <div :data-index="prop.index + '-mxLp'" class="fun-ico g-txt-hide" data-action="fun">互动</div>
       <div :id="prop.postInfo.articleId" class="fun-box">
-        <div :class="isLikedByUser ? 'fun-btn like allow liked':'fun-btn like allow'" :data-index="prop.index"
+        <div :class="isLikedByUserOrGuest ? 'fun-btn like allow liked':'fun-btn like allow'" :data-index="prop.index"
              data-action="like"
              data-likedtext="取消" data-liketext="赞" @click="postLike(prop.postInfo, $event.target)">
-          {{ isLikedByUser ? '取消' : '赞' }}
+          {{ isLikedByUserOrGuest ? '取消' : '赞' }}
         </div>
         <div :data-index="prop.index + '-mxLp'" class="fun-btn comment allow" data-action="comment" data-count="0"
              data-people="0">评论
@@ -22,13 +22,13 @@
     <div v-show="isLike" :id="'post-'+prop.postInfo.articleId+'-mxLp-like'"
          class="fun-area post-like g-clear-both show">
       <ul class="like-userslist g-right-flex">
-        <!-- 遍历 likeList，显示每个点赞用户 -->
-        <li v-for="item in prop.likeList" v-show="item.articleId === prop.postInfo.articleId"
-            :id="'post-'+prop.postInfo.articleId+'-like-me'"
-            :key="item.likeId"
-            :class="item.userId !== null ? 'like-name g-user-select' : 'like-name more'"
-            data-separator=",">
-          {{ item.userId === null ? guestCount + '位访客' : item.userName }}
+        <!-- 遍历格式化的点赞列表显示每个登录用户和访客统计 -->
+        <li v-for="name in displayNames" :key="name" class="like-name g-user-select" data-separator=",">
+          {{ name }}
+        </li>
+        <!-- 如果有访客点赞，显示访客统计 -->
+        <li v-if="guestCount > 0" class="like-name more">
+          {{ guestCount + '位访客' }}
         </li>
       </ul>
     </div>
@@ -39,7 +39,7 @@
 </template>
 
 <script lang="ts" setup>
-import {computed, nextTick, reactive, watch} from 'vue';
+import {computed, nextTick, reactive} from 'vue';
 import {timeUtils} from "@/store/TimeUtil";
 import {giveALike} from "@/api/likes";
 
@@ -58,15 +58,18 @@ const isLike = computed(() => {
   return prop.likeList.some(item => item.articleId === prop.postInfo.articleId);
 });
 
-// 计算游客人数
+// 计算登录用户的用户名列表
+const displayNames = computed(() => {
+  return prop.likeList
+      .filter(item => item.articleId === prop.postInfo.articleId && item.userId !== null)
+      .map(item => item.userName);
+});
+
+// 计算点赞的访客数量
 const guestCount = computed(() => {
-  let count = 0;
-  prop.likeList.forEach(item => {
-    if (item.userId === null && item.articleId === prop.postInfo.articleId) {
-      count++;
-    }
-  });
-  return count;
+  return prop.likeList
+      .filter(item => item.articleId === prop.postInfo.articleId && item.userId === null)
+      .length;
 });
 
 // 点赞参数
@@ -98,8 +101,10 @@ const page = reactive({
 });
 
 // 计算属性，判断当前用户是否已点赞
-const isLikedByUser = computed(() => {
-  return prop.likeList.some(item => item.userId === userId && item.articleId === prop.postInfo.articleId);
+const isLikedByUserOrGuest = computed(() => {
+  return prop.likeList.some(item => {
+    return (item.userId === userId ) && item.articleId === prop.postInfo.articleId;
+  });
 });
 
 // 点赞功能
@@ -114,18 +119,18 @@ const postLike = (info, button) => {
     // 发送点赞请求
     giveALike(likes).then(res => {
       nextTick(() => {
-        if (isLikedByUser.value) {
-          // 如果当前用户已点赞，则取消点赞
-          prop.likeList = prop.likeList.filter(item => !(item.userId === userId && item.articleId === info.articleId));
-        } else {
-          // 如果当前用户未点赞，则添加点赞
-          prop.likeList.push({
-            likeId: res.data.likeId,
-            articleId: info.articleId,
-            userId: userId,
-            userName: localStorage.getItem('userName')
-          });
-        }
+        // if (isLikedByUser.value) {
+        //   // 如果当前用户已点赞，则取消点赞
+        //   prop.likeList = prop.likeList.filter(item => !(item.userId === userId && item.articleId === info.articleId));
+        // } else {
+        //   // 如果当前用户未点赞，则添加点赞
+        //   prop.likeList.push({
+        //     likeId: res.data.likeId,
+        //     articleId: info.articleId,
+        //     userId: userId,
+        //     userName: localStorage.getItem('userName')
+        //   });
+        // }
 
         prop.initLikesList(); // 初始化点赞列表
 
