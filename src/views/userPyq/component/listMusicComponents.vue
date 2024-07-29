a21f96956e9383f3d858a3069f4f559f6adb153d
couei <669307582@qq.com>
2024-07-29 12:42:56 +0800

    feat: 自动部署配置

--- src/views/userPyq/component/listMusicComponents.vue
+++ src/views/userPyq/component/listMusicComponents.vue
@@ -27,8 +27,7 @@
 </template>
 <script lang="ts" setup>
 import {postActionProcessing} from "@/hooks/post.ts";
-import {ref} from "vue";
-import {defineProps} from "vue/dist/vue";
+import {ref, defineProps} from "vue";
 // 使用 defineProps 一次定义所有的 props
 // 定义 props 并指定 musicList 的类型为 Array
 const props = defineProps({
